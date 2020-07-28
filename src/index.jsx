42e3a90df584019cd033981ec7c7ef29e4c39e56
Romain Crestey <rcrestey@gmail.com>
2020-07-28 14:12:54 +0200

    correction

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import data from './_data/pokemon.json';
+import App from './classes/App';
+
 import './index.scss';
 
-const pokemon = <div>Pikachu</div>;
-
-ReactDOM.render(pokemon, document.getElementById('root'));
+ReactDOM.render(<App data={data} />, document.getElementById('root'));
