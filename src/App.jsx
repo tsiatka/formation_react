5ff9b4c0aa73b6c71d4a5e408eeb86ddfa02c9f6
Tom Siatka <tom.siatka@laposte.net>
2021-02-04 17:50:45 +0100

    classes -> hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+/*import React, { Component } from "react";
 
 import PokemonList from "./classes/PokemonList";
 import Trainer from "./classes/Trainer";
@@ -21,10 +21,10 @@ class App extends Component {
     this.setState({ selected: name });
   }
   capture(pokemon) {
-    this.setState({ captured: [...this.state.captured, pokemon] });
+    this.setState({ captured: [...captured, pokemon] });
   }
   remove(id) {
-    let copy = [...this.state.captured];
+    let copy = [...captured];
     copy.splice(id, 1);
     this.setState({ captured: copy });
   }
@@ -47,17 +47,17 @@ class App extends Component {
     types = [...new Set(types)];
 
     const filteredPokemons = data.filter((pokemon) =>
-      pokemon.types.map((el) => el.type.name).includes(this.state.selected)
+      pokemon.types.map((el) => el.type.name).includes(selected)
     );
 
     const pokemonsListe = (
       <PokemonList
         capture={this.capture}
-        data={this.state.selected ? filteredPokemons : data}
+        data={selected ? filteredPokemons : data}
       />
     );
 
-    const trainedPokemon = this.state.captured;
+    const trainedPokemon = captured;
 
     const trainer = (
       <Trainer
@@ -78,6 +78,74 @@ class App extends Component {
       </>
     );
   }
+}
+
+export default App;
+*/
+
+import React, { useState, useEffect } from "react";
+
+import PokemonList from "./classes/PokemonList";
+import Trainer from "./classes/Trainer";
+import Filters from "./classes/Filters";
+import fetchPokemon from "./utils/fetchPokemon";
+
+function App() {
+  const [selected, setSelected] = useState("");
+  const [captured, setCaptured] = useState([]);
+  const [data, setData] = useState([]);
+
+  function select(name) {
+    setSelected(name);
+  }
+  function capture(pokemon) {
+    setCaptured([...captured, pokemon]);
+  }
+  function remove(id) {
+    let copy = [...captured];
+    copy.splice(id, 1);
+    setCaptured(copy);
+  }
+
+  useEffect(() => {
+    fetchPokemon().then((e) => setData(e));
+  }, []);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  let types = data.flatMap((pokemon) => pokemon.types.map((t) => t.type.name));
+  types = [...new Set(types)];
+
+  const filteredPokemons = data.filter((pokemon) =>
+    pokemon.types.map((el) => el.type.name).includes(selected)
+  );
+
+  const pokemonsListe = (
+    <PokemonList capture={capture} data={selected ? filteredPokemons : data} />
+  );
+
+  const trainedPokemon = captured;
+
+  const trainer = (
+    <Trainer
+      name="Sacha"
+      address="Bourgpalette"
+      trainedPokemon={trainedPokemon}
+      remove={remove}
+    />
+  );
+
+  const filters = <Filters types={types} select={select} />;
+
+  return (
+    <>
+      {trainer}
+      {filters}
+      {pokemonsListe}
+    </>
+  );
 }
 
 export default App;
--- src/classes/TrainedPokemon.jsx
+++ src/classes/TrainedPokemon.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+/*import React, { Component } from "react";
 
 class TrainedPokemon extends Component {
   constructor() {
@@ -36,6 +36,29 @@ class TrainedPokemon extends Component {
       </li>
     );
   }
+}
+
+export default TrainedPokemon;
+*/
+import React, { useState } from 'react';
+
+function TrainedPokemon({ name, weight, src, id, remove }) {
+  const [exp, setExp] = useState(0);
+  const [nickname, setNickname] = useState('');
+
+    return (
+      <li
+        className="TrainedPokemon"
+        onMouseMove={() => setExp(prevExp => prevExp + 1)}
+      >
+        <div className="name">{name}</div>
+        <input placeholder="donner un surnom" value={nickname} onChange={(e) => setNickname(e.target.value)}></input>
+        <div className="weight">{weight} kg</div>
+        <div className="exp">{exp}</div>
+        {src && <img src={src} alt={name} />}
+        <button onClick={() => remove(id)}>relacher dans Kanto</button>
+      </li>
+    );
 }
 
 export default TrainedPokemon;
